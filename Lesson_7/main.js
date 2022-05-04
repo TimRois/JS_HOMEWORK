f8c4443f5f95e0d1df8400379110ac4ddd7101b2
TimRois <andrei_k99@mail.ru>
2022-05-04 06:16:22 +0300

    lesson 7

---
+++ Lesson_7/main.js
@@ -0,0 +1,36 @@
+function Cat(_name) {
+  
+  this.name = _name;
+  foodAmount = 50;
+  
+  formatFoodAmount = function () {
+    return foodAmount + ' гр ';
+  };
+
+  this.dailyNorm = function (_foodAmount) {
+    if (!arguments.length) return formatFoodAmount();
+
+    if (_foodAmount < 50) {
+      throw new Error('Значение должно быть > 50');
+    }
+
+    if (_foodAmount > 100) {
+      throw new Error('Значение должно быть < 100 ');
+    }
+
+    foodAmount = _foodAmount;
+  };
+
+  this.feed = function () {
+    console.log('Насыпаем в миску ' + this.dailyNorm() + 'корма');
+  };
+}
+
+var cat = new Cat('Kitty');
+console.log(cat.name);
+cat.feed();
+cat.dailyNorm(77);
+cat.feed();
+cat.dailyNorm(105);
+cat.feed();
+cat = null;
